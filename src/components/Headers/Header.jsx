ba56c93957be3d29fbc68cd160a01d272df00761
Hossein Moradi <ho3inmoradi1998@gmail.com>
2025-11-02 10:26:25 +0330

    update data file

--- src/components/Headers/Header.jsx
+++ src/components/Headers/Header.jsx
@@ -1,24 +1,94 @@
 import { MdLocationPin } from "react-icons/md";
-import {HiCalendar, HiSearch} from "react-icons/hi";
+import {HiCalendar, HiMinus, HiPlus, HiSearch} from "react-icons/hi";
+import {useState} from "react";
+import useOutsideClick from "../../hooks/useOutsideClick.js";
+import { forwardRef, useRef } from "react";
+import { DateRange } from "react-date-range";
+import { format } from "date-fns";
 
 function Header() {
+    const childRef = useRef(undefined);
+    const calendarRef = useRef(undefined);
+    const [destination, setDestination] = useState('');
+    const [showDropDown, setShowDropDown] = useState(false);
+    const [showCalendar, setShowCalendar] = useState(false);
+    const [options, setOptions] = useState({
+        Adult: 1,
+        Children: 1,
+        Room: 1
+    });
+    const [range, setRange] = useState([
+        {
+            startDate: new Date(),
+            endDate: new Date(),
+            key: "selection",
+        },
+    ]);
+
+    const handleDestinationChange = (e) => {
+        setDestination(e.target.value)
+    }
+
+    const handleShowDropDown = () => {
+        setShowDropDown(!showDropDown);
+    }
+
+    const handleShowCalendar = () => {
+        setShowCalendar(!showCalendar);
+    }
+
+    useOutsideClick(childRef, handleShowDropDown);
+
+    useOutsideClick(calendarRef, handleShowCalendar);
+
+    const updateOptions = (key, value) => {
+        setOptions((prevOption) => ({
+            ...prevOption,
+            [key]: value,
+        }));
+    };
+
     return(
         <div className="header">
             <div className="headerSearch">
                 <div className="headerSearchItem">
                     <MdLocationPin className="headerIcon locationIcon" />
-                    <input type="text" placeholder="Where to go?" className="headerSearchInput" name="destination" id="destination" />
+                    <input type="text"
+                           placeholder="Where to go?"
+                           className="headerSearchInput"
+                           name="destination"
+                           id="destination"
+                           onChange={handleDestinationChange}
+                           value={destination}
+                    />
                     <span className="seperator"></span>
                 </div>
                 <div className="headerSearchItem">
                     <HiCalendar className="headerIcon dateIcon"/>
-                    <div className="dateDropDown">2023/06/23</div>
+                    <div className="dateDropDown" onClick={handleShowCalendar}>
+                        {format(range[0].startDate, "yyyy-MM-dd")} to {format(range[0].endDate, "yyyy-MM-dd")}
+                    </div>
+                    {showCalendar && <div ref={calendarRef} className="date">
+                        <DateRange
+                            minDate={new Date()}
+                            editableDateInputs={true}
+                            onChange={(item) => setRange([item.selection])}
+                            moveRangeOnFirstSelection={false}
+                            ranges={range}
+                        />
+                    </div>}
                     <span className="seperator"></span>
                 </div>
                 <div className="headerSearchItem">
-                    <div id="optionDropdown">
-                        1 adult &bull; 2 children &bull; 1 room
+                    <div id="optionDropdown" onClick={handleShowDropDown}>
+                        {Object.entries(options).map(([key, value], index) => (
+                            <span key={key}>
+                                <strong>{key}</strong>: {value}
+                                {index < Object.keys(options).length - 1 && <span> &bull; </span>}
+                            </span>
+                        ))}
                     </div>
+                    {showDropDown && <GuestOptions options={options} onUpdateOptions={updateOptions} ref={childRef} />}
                     <span className="seperator"></span>
                 </div>
                 <div className="headerSearchItem">
@@ -32,3 +102,32 @@ function Header() {
 }
 
 export default Header;
+
+// eslint-disable-next-line react/display-name
+const GuestOptions = forwardRef(({ options, onUpdateOptions }, ref) => {
+    return (
+        <div className="guestOptions" ref={ref}>
+            {Object.entries(options).map(([key, value]) => (
+                <GuestOptionItem key={key} optionName={key} optionValue={value} onUpdateOptions={onUpdateOptions}/>))}
+        </div>
+)
+    ;
+});
+
+
+function GuestOptionItem({optionName, optionValue, onUpdateOptions}) {
+    return (
+        <div className="guestOptionItem">
+            <span className="optionText">{ optionName }</span>
+            <div className="optionCounter">
+                <button className="optionCounterBtn" disabled={optionValue < 1}>
+                    <HiMinus className="icon" onClick={() => onUpdateOptions(optionName, optionValue - 1)}/>
+                </button>
+                <span className="optionCounterNumber">{optionValue}</span>
+                <button className="optionCounterBtn">
+                    <HiPlus className="icon" onClick={() => onUpdateOptions(optionName, optionValue + 1)}/>
+                </button>
+            </div>
+        </div>
+    )
+}
---
+++ src/hooks/useOutsideClick.js
@@ -0,0 +1,19 @@
+import {useEffect} from "react";
+
+function useOutsideClick(ref, callback) {
+    useEffect(() => {
+        function handleOutsideClick(event) {
+            if (ref.current && !ref.current.contains(event.target)) {
+                callback(event);
+            }
+        }
+
+        document.addEventListener('mousedown', handleOutsideClick);
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        }
+    },[ref, callback])
+}
+
+export default useOutsideClick;
+
